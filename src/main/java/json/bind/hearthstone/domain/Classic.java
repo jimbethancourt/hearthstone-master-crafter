f9e4f0b1018d29a7e8d86330127226de2032fa1c
jimbethancourt <bigjimb0>
2015-08-16 14:06:58 -0500

    removed commented out subclass code and made subclasses package private

--- src/main/java/json/bind/hearthstone/domain/Classic.java
+++ src/main/java/json/bind/hearthstone/domain/Classic.java
@@ -1,43 +1,6 @@
 
 package json.bind.hearthstone.domain;
 
-import java.util.List;
+class Classic extends Card{
 
-public class Classic extends Card{
-   	/*private String id;
-   	private List<Mechanics> mechanics;
-   	private String name;
-   	private String text;
-   	private String type;
-
- 	public String getId(){
-		return this.id;
-	}
-	public void setId(String id){
-		this.id = id;
-	}
- 	public List<Mechanics> getMechanics(){
-		return this.mechanics;
-	}
-	public void setMechanics(List<Mechanics> mechanics){
-		this.mechanics = mechanics;
-	}
- 	public String getName(){
-		return this.name;
-	}
-	public void setName(String name){
-		this.name = name;
-	}
- 	public String getText(){
-		return this.text;
-	}
-	public void setText(String text){
-		this.text = text;
-	}
- 	public String getType(){
-		return this.type;
-	}
-	public void setType(String type){
-		this.type = type;
-	}*/
 }
--- src/main/java/json/bind/hearthstone/domain/CurseOfNaxxramas.java
+++ src/main/java/json/bind/hearthstone/domain/CurseOfNaxxramas.java
@@ -1,36 +1,6 @@
 
 package json.bind.hearthstone.domain;
 
-import java.util.List;
+class CurseOfNaxxramas extends Card {
 
-public class CurseOfNaxxramas extends Card {
-   	/*private Number health;
-   	private String id;
-   	private String name;
-   	private String type;
-
- 	public Number getHealth(){
-		return this.health;
-	}
-	public void setHealth(Number health){
-		this.health = health;
-	}
- 	public String getId(){
-		return this.id;
-	}
-	public void setId(String id){
-		this.id = id;
-	}
- 	public String getName(){
-		return this.name;
-	}
-	public void setName(String name){
-		this.name = name;
-	}
- 	public String getType(){
-		return this.type;
-	}
-	public void setType(String type){
-		this.type = type;
-	}*/
 }
