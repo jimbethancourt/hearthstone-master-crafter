053f319b9d2ad58b02e8eef7997a9eccba6511b2
jimbethancourt <jimbethancourt@gmail.com>
2015-08-23 20:03:20 -0500

    Adding The Grand Tournament set

--- src/main/java/json/bind/hearthstone/domain/CardUniverse.java
+++ src/main/java/json/bind/hearthstone/domain/CardUniverse.java
@@ -1,9 +1,11 @@
 
 package json.bind.hearthstone.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties({"Hero Skins", "Tavern Brawl", "Debug", "Missions", "Credits", "System"})
 public class CardUniverse{
 	@JsonProperty("Basic")
    	private List<Basic> basic;
@@ -11,22 +13,16 @@ public class CardUniverse{
    	private List<BlackrockMountain> blackrockMountain;
 	@JsonProperty("Classic")
    	private List<Classic> classic;
-	@JsonProperty("Credits")
-   	private List<Credits> credits;
 	@JsonProperty("Curse of Naxxramas")
    	private List<CurseOfNaxxramas> curseOfNaxxramas;
-	@JsonProperty("Debug")
-   	private List<Debug> debug;
 	@JsonProperty("Goblins vs Gnomes")
    	private List<GoblinsVsGnomes> goblinsVsGnomes;
-	@JsonProperty("Missions")
-   	private List<Missions> missions;
 	@JsonProperty("Promotion")
    	private List<Promotion> promotion;
 	@JsonProperty("Reward")
    	private List<Reward> reward;
-	@JsonProperty("System")
-   	private List<SystemCardSet> system;
+	@JsonProperty("The Grand Tournament")
+	private List<TheGrandTournament> theGrandTournament;
 
  	public List<Basic> getBasic(){
 		return this.basic;
@@ -46,35 +42,18 @@ public class CardUniverse{
 	public void setClassic(List<Classic> classic){
 		this.classic = classic;
 	}
- 	public List<Credits> getCredits(){
-		return this.credits;
-	}
-	public void setCredits(List<Credits> credits){
-		this.credits = credits;
-	}
+
  	public List<CurseOfNaxxramas> getCurseOfNaxxramas(){
 		return this.curseOfNaxxramas;
 	}
 	public void setCurseOfNaxxramas(List<CurseOfNaxxramas> curseOfNaxxramas){
 		this.curseOfNaxxramas = curseOfNaxxramas;
 	}
- 	public List<Debug> getDebug(){
-		return this.debug;
-	}
-	public void setDebug(List<Debug> debug){
-		this.debug = debug;
-	}
  	public List<GoblinsVsGnomes> getGoblinsVsGnomes(){
 		return this.goblinsVsGnomes;
 	}
 	public void setGoblinsVsGnomes(List<GoblinsVsGnomes> goblinsVsGnomes){
 		this.goblinsVsGnomes = goblinsVsGnomes;
-	}
- 	public List<Missions> getMissions(){
-		return this.missions;
-	}
-	public void setMissions(List<Missions> missions){
-		this.missions = missions;
 	}
  	public List<Promotion> getPromotion(){
 		return this.promotion;
@@ -88,10 +67,12 @@ public class CardUniverse{
 	public void setReward(List<Reward> reward){
 		this.reward = reward;
 	}
- 	public List<SystemCardSet> getSystem(){
-		return this.system;
+
+	public List<TheGrandTournament> getTheGrandTournament() {
+		return theGrandTournament;
 	}
-	public void setSystem(List<SystemCardSet> system){
-		this.system = system;
+
+	public void setTheGrandTournament(List<TheGrandTournament> theGrandTournament) {
+		this.theGrandTournament = theGrandTournament;
 	}
 }
